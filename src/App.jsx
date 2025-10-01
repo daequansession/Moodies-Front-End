868e04b57684615d1d60748d25b60ce3448c600b
Dylan Tai <dylztai@gmail.com>
2025-10-01 15:06:59 -0400

    added .css files

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,8 @@ import { useState, useEffect } from "react";
 import { Route, Routes } from "react-router";
 
 import * as moodService from "./services/MoodService.js";
-import MoodDetail from "./components/MoodDetail/MoodDetail.jsx";
 import MoodList from "./components/MoodList/MoodList.jsx";
-import MoodForm from "./components/MoodForm/MoodForm.jsx";
+import Landing from "./components/Landing/Landing.jsx"
 import "./App.css";
 
 
@@ -22,12 +21,7 @@ const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<MoodList moods={moods} />} />
-        <Route path="/moods/new" element={<MoodForm setMoods={setMoods} moods={moods}/>} />
-        <Route
-          path="/moods/:moodId"
-          element={<MoodDetail setMoods={setMoods} moods={moods}/>}
-        />
+        <Route path="/" element={ user ? <MoodList /> : <Landing />}/>
       </Routes>
     </>
   );
