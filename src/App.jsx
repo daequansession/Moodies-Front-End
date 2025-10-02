ac97bf7b2543c233a76c0b7af9457f0f6965e4ba
Dylan Tai <dylztai@gmail.com>
2025-10-02 10:48:45 -0400

    repushed old code again

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { useState, useEffect } from "react";
 import { Route, Routes } from "react-router";
-<<<<<<< HEAD
-
 import * as moodService from "./services/MoodService.js";
 import MoodDetail from "./components/MoodDetail/MoodDetail.jsx";
 import MoodList from "./components/MoodList/MoodList.jsx";
@@ -11,15 +9,6 @@ import "./App.css";
 
 const App = () => {
   const [moods, setMoods] = useState([]);
-=======
-import MoodDetail from "./components/MoodDetail/MoodDetail.jsx";
-import MoodList from "./components/MoodList/MoodList.jsx";
-import MoodForm from "./components/MoodForm/MoodForm.jsx";
-import "./App.css";
-
-const App = () => {
-  const [mood, setMoods] = useState([]);
->>>>>>> 7c99832 (Init commit:)
 
   useEffect(() => {
     const getMoods = async () => {
@@ -33,17 +22,10 @@ const App = () => {
     <>
       <Routes>
         <Route path="/" element={<MoodList moods={moods} />} />
-<<<<<<< HEAD
         <Route path="/moods/new" element={<MoodForm setMoods={setMoods} moods={moods}/>} />
         <Route
           path="/moods/:moodId"
           element={<MoodDetail setMoods={setMoods} moods={moods}/>}
-=======
-        <Route path="/moods/new" element={<MoodForm setMoods={setMoods} />} />
-        <Route
-          path="/moods/:moodId"
-          element={<MoodDetail setMoods={setMoods} />}
->>>>>>> 7c99832 (Init commit:)
         />
       </Routes>
     </>
