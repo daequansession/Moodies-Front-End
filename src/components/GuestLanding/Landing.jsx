66549acd42deaec799e33a70be04e9f89cf637ce
Dylan Tai <dylztai@gmail.com>
2025-10-02 10:50:45 -0400

    Removed Dashboard + Renamed Landing -> GuestLanding

---
+++ src/components/GuestLanding/Landing.jsx
@@ -0,0 +1,14 @@
+const Landing = () => {
+  return (
+    <main>
+      <h1>Hello, you are on the landing page for visitors.</h1>
+      <p>Sign up now, or sign in to see your super secret dashboard!</p>
+    </main>
+  );
+};
+
+<<<<<<< HEAD
+export default Landing;
+=======
+export default Landing;
+>>>>>>> 7c99832 (Init commit:)
