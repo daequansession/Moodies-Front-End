c1b05bfd1cd0957a7d8935542ac6294ee57184ab
Dylan Tai <dylztai@gmail.com>
2025-10-02 14:08:15 -0400

    Working as of right now, many issues still

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,6 +1,6 @@
 import { useState, useContext } from "react";
 import { useNavigate } from "react-router";
-import { signUp } from "../../services/authService.js";
+import { signUp } from "../../services/AuthService.js";
 import { UserContext } from "../../contexts/UserContext.jsx";
 import "./SignUpForm.css";
 
