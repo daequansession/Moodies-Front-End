10775505545643fbadebcbd8a7e37678a0ddb803
Sara Mattina <smattina@gmail.com>
2025-10-06 11:29:04 -0400

    add footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="in-case-of-emergency">
+      <p>*If you are experiencing an emergency, please call 911 or go to the nearest emergency room</p>
+    </div>
+  )
+}
+
+export default Footer;
